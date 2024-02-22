5bedb8c06f38e6d0244be50e3c8b991955681c93
nxitik <nrzaveri@wpi.edu>
2024-02-22 13:51:27 -0500

    Add data validation configuration and pipeline

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from carPred import logger
 from carPred.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
+from carPred.pipeline.Stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f"Starting {STAGE_NAME}.... started")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"Completed {STAGE_NAME}.... completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f"Starting {STAGE_NAME}.... started")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f"Completed {STAGE_NAME}.... completed")
 except Exception as e:
